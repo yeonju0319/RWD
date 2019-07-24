385679348c305df352c11dd2e15d1502ee6527c7
yeonju0319 <joojoo0319@gmail.com>
2019-07-24 11:50:45 +0900

    메인섹션제목공통디자인완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -28,19 +28,28 @@ if(viewport.matches){
 
   li.on('click keydown', function(e){
     if(e.type === 'click' || (e.type === 'keydown' && e.keyCode === 13)){
-      li.removeClass('menu-act');
-      span.removeClass('icon-minus').addClass('icon-plus');
-      span.attr('aria-expanded', 'false');
-      $(this).find('.menu-item').removeClass('icon-plus').addClass('icon-minus');
-      $(this).attr('aria-expanded', 'true');
-      $(this).addClass('menu-act');
+      // li.removeClass('menu-act');
+      // span.removeClass('icon-minus').addClass('icon-plus');
+      // $(this).find('.menu-item').removeClass('icon-plus').addClass('icon-minus');
+      // $(this).addClass('menu-act');
+      if($(this).hasClass('menu-act')){
+        $(this).removeClass('menu-act');
+        span.removeClass('icon-minus').addClass('icon-plus');
+        span.attr('aria-expanded', 'false');
+      }else{
+        li.removeClass('menu-act');
+        span.removeClass('icon-minus').addClass('icon-plus');
+        span.attr('aria-expanded', 'false');
+        $(this).find('.menu-item').removeClass('icon-plus').addClass('icon-minus');
+        $(this).find('.menu-item').attr('aria-expanded', 'true');
+        $(this).addClass('menu-act');
+      }
     }
   });
   menu.on('focusout', function(){
     nav.removeClass('is-act');
-});
-menu.on('focusin', function(){
-    nav.addClass('is-act');
-});
-
+  });
+  menu.on('focusin', function(){
+      nav.addClass('is-act');
+  });
 }
